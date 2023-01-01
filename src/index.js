beb5451a42b8d2dbecc3a342740df1b67dea69ba
894kun <heart_warming_movie@yahoo.co.jp>
2023-01-01 08:23:34 +0000

    Update 24 files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+const onClickAdd = () => {
+    const task = document.getElementById("task_add_txt").value;
+    
+    const li = document.createElement('li');
+    console.log(li);
+    
+    const div = document.createElement('div');
+    const pTag = document.createElement('p');
+    const compBtn = document.createElement('button');
+    const delBtn = document.createElement('button');
+}
+
+document.getElementById("task_add_btn").addEventListener("click", () => onClickAdd());
